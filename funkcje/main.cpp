39bb48df97f67fc6ca6cf372f796702dce972e0c
dwierzbicki11 <117636569+dwierzbicki11@users.noreply.github.com>
2024-04-01 21:02:29 +0200

    tworzenie klas jako zadań

--- funkcje/main.cpp
+++ funkcje/main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
+class zad1{
+    public zad1(){
+
+    }
+}
+class zad2{}
+class zad3{}
+class zad4{}
+class zad5{}
+class zad6{}
+class zad7{}
+class zad8{}
+class zad9{}
+class zad10{}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
     return 0;
 }
